7b0702be1ac6b351dd2a1c06641c0ecb6506221c
Alexander Zavoykin <aazavoykin@gmail.com>
2019-08-01 12:22:45 +0300

    Accessory modifiers are corrected

--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -3,10 +3,10 @@ package main.java;
 import javax.swing.*;
 import java.awt.*;
 
-class Util {
+public class Util {
 
     // returns icon scaled to the defined size
-    static ImageIcon scaleIcon(String path, int size) {
+    public static ImageIcon scaleIcon(String path, int size) {
         ImageIcon icon = new ImageIcon(path);
         Image iconImage = icon.getImage();
         Image scaledIconImage = iconImage.getScaledInstance(size, size, java.awt.Image.SCALE_SMOOTH);
